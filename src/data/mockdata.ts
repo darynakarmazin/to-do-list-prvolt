bbd62eafba92760a839d4d6732df2138f7b14f37
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-03-06 15:27:03 +0200

    Added mockData for demonstration app

---
+++ src/data/mockdata.ts
@@ -0,0 +1,6 @@
+export const mockData = [
+  { id: "0", text: "Learn HTML/CSS and JavaScript", completed: true },
+  { id: "1", text: "Master React", completed: false },
+  { id: "2", text: "Discover Redux", completed: false },
+  { id: "3", text: "Build amazing apps", completed: false },
+];
--- src/redux/tasksSlice.ts
+++ src/redux/tasksSlice.ts
@@ -1,12 +1,8 @@
 import { createSlice, PayloadAction, nanoid } from "@reduxjs/toolkit";
 import { Task } from "../types/types";
+import { mockData } from "../data/mockdata";
 
-const tasksInitialState: Task[] = [
-  { id: "0", text: "Learn HTML/CSS and JavaScript", completed: true },
-  { id: "1", text: "Master React", completed: false },
-  { id: "2", text: "Discover Redux", completed: false },
-  { id: "3", text: "Build amazing apps", completed: false },
-];
+const tasksInitialState: Task[] = mockData || [];
 
 const tasksSlice = createSlice({
   name: "tasks",
