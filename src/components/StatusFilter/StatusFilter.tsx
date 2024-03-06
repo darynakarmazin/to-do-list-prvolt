5a8d48ef1ed0a30333964a9d06e726b14d695ea3
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-03-06 12:39:21 +0200

    Added StatusFilter component

--- src/components/StatusFilter/StatusFilter.tsx
+++ src/components/StatusFilter/StatusFilter.tsx
@@ -1,16 +1,36 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../Button/Button";
 import { statusFilters } from "../../redux/constants";
 import { getStatusFilter } from "../../redux/selectors";
+import { setStatusFilter } from "../../redux/filtersSlice";
 
 export const StatusFilter = () => {
+  const dispatch = useDispatch();
   const filter = useSelector(getStatusFilter);
+
+  const handleFilterChange = (filter: string) =>
+    dispatch(setStatusFilter(filter));
 
   return (
     <div className="flex gap-4">
-      <Button selected={filter === statusFilters.all}>All</Button>
-      <Button selected={filter === statusFilters.active}>Active</Button>
-      <Button selected={filter === statusFilters.completed}>Completed</Button>
+      <Button
+        selected={filter === statusFilters.all}
+        onClick={() => handleFilterChange(statusFilters.all)}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === statusFilters.active}
+        onClick={() => handleFilterChange(statusFilters.active)}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === statusFilters.completed}
+        onClick={() => handleFilterChange(statusFilters.completed)}
+      >
+        Completed
+      </Button>
     </div>
   );
 };
